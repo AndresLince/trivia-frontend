859e839b162cd96e32d860477cd5b1d3761da7f1
AndresLince <lynxdarmatron@gmail.com>
2023-02-24 19:59:53 -0500

    Add user shell to the app routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'user',
+    loadChildren: () => import('./user/feature/user-shell/user-shell.module').then(m => m.UserShellModule)
+  }
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
