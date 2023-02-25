ffc621a31111b118356f9825b2e22bfced6099c7
AndresLince <lynxdarmatron@gmail.com>
2023-02-25 11:08:34 -0500

    Add input error messages to user create form

--- src/app/user/feature/user-create-form/user-create-form.component.ts
+++ src/app/user/feature/user-create-form/user-create-form.component.ts
@@ -18,8 +18,37 @@ export class UserCreateFormComponent {
       ]
     ]
   });
+  get username() {
+    return this.form.get('username');
+  }
+
   constructor(public formBuilder: FormBuilder) {}
+
   showSubmitButton(): boolean {
-    return !this.form.valid || this.submitted
+    return !this.form.valid || this.submitted;
+  }
+
+  hasErrors() {
+    if (this.form.get('username')?.errors && this.form.get('username')?.dirty) {
+      return true;
+    }
+    return false;
+  }
+
+  getErrorMessage(): string {
+    const errors = this.form.get('username')?.errors
+    if (!errors) {
+      return "";
+    }
+    if (errors['required']) {
+      return "El nombre de usuario debe ser mas largo";
+    }
+    if (errors['minlength']) {
+      return "El nombre de usuario debe ser mas largo";
+    }
+    if (errors['maxlength']) {
+      return "El nombre de usuario debe ser mas corto";
+    }
+    return "";
   }
 }
