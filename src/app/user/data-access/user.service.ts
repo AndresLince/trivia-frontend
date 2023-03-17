a9d6a1e12a5c620d6e8c793eafbc7e0e986d7512
AndresLince <lynxdarmatron@gmail.com>
2023-03-17 17:51:19 -0500

    Add signup function to user service

--- src/app/user/data-access/user.service.ts
+++ src/app/user/data-access/user.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { Observable, tap } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { ConfigService } from 'src/app/shared/data-access/config.service';
+import { UserSignUp } from './user.interfaces';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+    private configService: ConfigService
+  ) { }
+
+  signUp(formData: UserSignUp): Observable<any> {
+    return this.http.post(`${this.configService.getConfig('apiUrl') }/auth/signup`, formData).pipe(
+      tap((resp: any) => {
+        localStorage.setItem('token', resp.token);
+      })
+    );
+  }
 }
