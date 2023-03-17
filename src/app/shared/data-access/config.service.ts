10bb60d348b82e70036416bf8205c35491f50c84
AndresLince <lynxdarmatron@gmail.com>
2023-03-17 05:53:32 -0500

    Add getConfig function to configService

--- src/app/shared/data-access/config.service.ts
+++ src/app/shared/data-access/config.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ConfigService {
 
-  constructor() { }
+  public environmentVariables: any = {};
+  constructor() {
+    const environmentVariables = Object.entries(environment);
+
+    environmentVariables.forEach((element) => {
+      const key = element[0];
+      const value = element[1];
+      this.environmentVariables[key] = value;
+    });
+  }
+
+  getConfig(key: string): string {
+    return this.environmentVariables[key] || '';
+  }
 }
