71f4397948f4585e423fba9c93b781c2e8f72e9f
AndresLince <lynxdarmatron@gmail.com>
2023-03-20 15:28:55 -0500

    Add routing to category question form module

--- src/app/trivia/feature/category-question-form/category-question-form.module.ts
+++ src/app/trivia/feature/category-question-form/category-question-form.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CategoryQuestionFormRoutingModule } from './category-question-form-routing.module';
 import { CategoryQuestionFormComponent } from './category-question-form.component';
-
-
 
 @NgModule({
   declarations: [
     CategoryQuestionFormComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    CategoryQuestionFormRoutingModule
   ]
 })
 export class CategoryQuestionFormModule { }
