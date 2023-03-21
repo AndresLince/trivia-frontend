0abe8d8ca4e7e839c6329e7dedd05be12ed7a116
AndresLince <lynxdarmatron@gmail.com>
2023-03-21 06:41:48 -0500

    Add trivia routes and auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './shared/guards/auth.guard';
 
 const routes: Routes = [
   {
+    path: '',
+    redirectTo: 'trivia/category-question',
+    pathMatch: 'full',
+  },
+  {
     path: 'user',
     loadChildren: () => import('./user/feature/user-shell/user-shell.module').then(m => m.UserShellModule)
+  },
+  {
+    path: 'trivia',
+    canActivate: [ AuthGuard ],
+    loadChildren: () => import('./trivia/feature/trivia-shell/trivia-shell.module').then(m => m.TriviaShellModule)
   }
 ]
 
