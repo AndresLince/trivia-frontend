3d140089f01eb13f88ff5a27f5c485bdd9ad5c4d
AndresLince <lynxdarmatron@gmail.com>
2023-03-21 06:01:19 -0500

    Add utils service

---
+++ src/app/shared/data-access/utils.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UtilsService {
+
+  constructor() { }
+
+  get token(): string {
+    return localStorage.getItem('token') || '';
+  }
+
+  get headers() {
+    return {
+      headers: {
+        'x-token': this.token
+      }
+    };
+  }
+}
