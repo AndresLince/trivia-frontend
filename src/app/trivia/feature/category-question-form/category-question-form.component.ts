b231287b52e5ebdad3414ee421e10890869df6fe
AndresLince <lynxdarmatron@gmail.com>
2023-03-24 17:56:46 -0500

    Add question category service to question category component

--- src/app/trivia/feature/category-question-form/category-question-form.component.ts
+++ src/app/trivia/feature/category-question-form/category-question-form.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CategoryQuestionService } from '../../data-access/category-question.service';
 
 @Component({
   selector: 'app-category-question-form',
   templateUrl: './category-question-form.component.html',
   styleUrls: ['./category-question-form.component.css']
 })
-export class CategoryQuestionFormComponent {
+export class CategoryQuestionFormComponent implements OnInit {
+  questionCategories = [];
 
+  constructor(
+    private categoryQuestionService: CategoryQuestionService
+  ) { }
+
+  ngOnInit() {
+    this.categoryQuestionService.getAll().subscribe(response => {
+      this.questionCategories = response.data;
+      console.log(this.questionCategories)
+    });
+  }
 }
