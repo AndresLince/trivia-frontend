782b30a0add3f7a259d739e12eb1f0318bc6bb4b
AndresLince <lynxdarmatron@gmail.com>
2023-04-03 06:31:17 -0500

    Add question route

--- src/app/trivia/feature/trivia-shell/trivia-shell-routing.module.ts
+++ src/app/trivia/feature/trivia-shell/trivia-shell-routing.module.ts
@@ -5,6 +5,10 @@ const routes: Routes = [
   {
     path: 'category-question',
     loadChildren: () => import('../category-question-form/category-question-form.module').then(m => m.CategoryQuestionFormModule)
+  },
+  {
+    path: 'question',
+    loadChildren: () => import('../question/question.module').then(m => m.QuestionModule)
   }
 ];
 
