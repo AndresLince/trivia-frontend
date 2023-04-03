c6816f15d747b6ebbd7796ea40e1e4879256d441
AndresLince <lynxdarmatron@gmail.com>
2023-04-03 06:29:14 -0500

    Add trivia service create function

--- src/app/trivia/data-access/trivia.service.ts
+++ src/app/trivia/data-access/trivia.service.ts
@@ -1,9 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ConfigService } from 'src/app/shared/data-access/config.service';
+import { QuestionCategory } from './questionCategory.interface';
+import { UtilsService } from 'src/app/shared/data-access/utils.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TriviaService {
 
-  constructor() { }
+  constructor(
+    private configService: ConfigService,
+    private http: HttpClient,
+    private utilsService: UtilsService,
+  ) { }
+
+  create(formData: QuestionCategory): Observable<any> {
+    return this.http.post(`${this.configService.getConfig('apiUrl') }/trivia`, formData, this.utilsService.headers);
+  }
 }
