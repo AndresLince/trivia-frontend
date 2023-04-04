2563bea9879b2ecec602f6b3dbc84f5649c93ac1
AndresLince <lynxdarmatron@gmail.com>
2023-04-04 06:22:08 -0500

    Add Forms module and Reactive forms module to question module

--- src/app/trivia/feature/question/question.module.ts
+++ src/app/trivia/feature/question/question.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { QuestionRoutingModule } from './question-routing.module';
 import { QuestionComponent } from './question.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -11,7 +12,9 @@ import { QuestionComponent } from './question.component';
   ],
   imports: [
     CommonModule,
-    QuestionRoutingModule
+    QuestionRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
   ]
 })
 export class QuestionModule { }
