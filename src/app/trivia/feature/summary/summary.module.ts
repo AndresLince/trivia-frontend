24a0e51e88545e92d4ef3abee71c9a5f2ba5519c
AndresLince <lynxdarmatron@gmail.com>
2023-04-13 06:22:01 -0500

    Add summary component

--- src/app/trivia/feature/summary/summary.module.ts
+++ src/app/trivia/feature/summary/summary.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SummaryRoutingModule } from './summary-routing.module';
+import { SummaryComponent } from './summary.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SummaryComponent
+  ],
   imports: [
     CommonModule,
     SummaryRoutingModule
