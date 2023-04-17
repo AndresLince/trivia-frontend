873527c0b80d1dad18eda14b68520747dead6e42
AndresLince <lynxdarmatron@gmail.com>
2023-04-17 06:42:21 -0500

    Add on click functionality to play again

--- src/app/trivia/feature/summary/summary.component.ts
+++ src/app/trivia/feature/summary/summary.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { TriviaService } from '../../data-access/trivia.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-summary',
@@ -10,12 +11,17 @@ export class SummaryComponent {
   public score: number = 0;
 
   constructor(
-    public triviaService: TriviaService
+    public triviaService: TriviaService,
+    private router: Router,
   ) {
     this.triviaService.getSummary().subscribe({
       next: (response) => {
         this.score = response.score;
       },
     })
+  }
+
+  handleOnClick() {
+    this.router.navigateByUrl('trivia/category-question');
   }
 }
