4824000a423761a1ac3e3e38f5099988d92c4ad4
AndresLince <lynxdarmatron@gmail.com>
2023-04-21 06:47:36 -0500

    Add scores-list input and html

--- src/app/trivia/ui/scores-list/scores-list.component.ts
+++ src/app/trivia/ui/scores-list/scores-list.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Score } from '../../data-access/scores.interface';
 
 @Component({
   selector: 'app-scores-list',
   templateUrl: './scores-list.component.html',
   styleUrls: ['./scores-list.component.css']
 })
-export class ScoresListComponent {
-
+export class ScoresListComponent{
+  @Input() scores: Score[] = [];
 }
